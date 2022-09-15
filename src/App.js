ba514183203445af9601da34284175a80b0fafd0
Siri Sollerud <sirisoll@uio.no>
2022-09-15 22:21:08 +0200

    stylized game over

--- src/App.js
+++ src/App.js
@@ -147,8 +147,8 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if(state.highScore > 0) {
-      localStorage.setItem("highscore", state.highScore)
+    if (state.highScore > 0) {
+      localStorage.setItem("highscore", state.highScore);
     }
   }, [state.highScore]);
 
@@ -231,9 +231,9 @@ function App() {
       {state.strikes >= 3 && state.currentQuestion && (
         <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/70 text-white flex justify-center items-center text-center">
           <div>
-            <p className="text-6xl mb-4 font-bold">3 strikes: you're out!</p>
+            <p className="text-6xl mb-4 font-bold">GAME OVER</p>
             <p>
-              Your score:{" "}
+              SCORE:{" "}
               <span className="text-amber-400">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -245,18 +245,17 @@ function App() {
                 >
                   <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                 </svg>
-                {state.points}
+                <span style={{fontWeight: 'bold'}}>{state.points}</span>
               </span>
             </p>
 
-            <p className="mb-5">Your all-time high score: {state.highScore}</p>
+            <p className="mb-5">HIGH SCORE: <span style={{fontWeight: 'bold'}}>{state.highScore}</span></p>
 
             {/* TODO: if collection is 140/151 at end of turn, next turn will just be 140 as well --> fetch again? */}
-            <button
-              onClick={() => dispatch({ type: "startPlaying" })}
-              className="text-white bg-gradient-to-b from-indigo-500 to-indigo-600 px-4 py-3 rounded text-lg font-bold"
-            >
-              Play again
+            <button onClick={() => dispatch({ type: "startPlaying" })} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Play Again
+              </span>
             </button>
           </div>
         </div>
